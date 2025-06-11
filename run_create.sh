bdf20790d0b03f710abfda6ceec78de3b717158e
Mafoin <marcusfoin1@gmail.com>
2025-06-11 14:44:35 +0200

    recall

--- run_create.sh
+++ run_create.sh
@@ -11,7 +11,7 @@ conda activate strainminer
 echo "=== Step 1: Creating CSV Matrices ==="
 for threshold in 0.5 0.7 0.8 0.9 1; do
     echo "Processing threshold: $threshold"
-    if python create_csv/process_bam_folder.py bam . --threshold $threshold > res$threshold.txt ; then
+    if python create_csv/process_bam_folder.py bam . --threshold $threshold ; then
         echo "✅ CSV creation completed successfully for $threshold!"
     else
         echo "❌ CSV creation failed for threshold $threshold"
